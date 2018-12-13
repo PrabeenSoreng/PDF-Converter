8e739b1f484de4511e695337f669372df03bc357
Prabeen <prabeensoreng2119@gmail.com>
2018-12-13 23:21:20 +0530

    Cross Origin Resource Sharing added

--- PDF-backend/server.js
+++ PDF-backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const word = require('./routes/WordRoutes');
 
 const app = express();
+app.use(cors());
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/converter', word);
 
