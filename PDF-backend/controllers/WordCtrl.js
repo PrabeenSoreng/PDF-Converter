167224125ec8a289baa97c224eea793f397ae2ab
Prabeen <prabeensoreng2119@gmail.com>
2018-12-14 20:59:31 +0530

    sending file to the frontend

--- PDF-backend/controllers/WordCtrl.js
+++ PDF-backend/controllers/WordCtrl.js
@@ -1,20 +1,25 @@
 const word2pdf = require('word2pdf');
 const fs = require('fs');
+const path = require('path');
 
 const helper = require('../helper/helper');
 
 exports.WordToPdf = (req, res) => {
     try {
         const base64file = req.body.file.split(';base64,').pop();
-        console.log(base64file);
         fs.writeFile(`./uploads/${req.body.name}`, base64file, 'base64', async(err) => {
             if (err) console.log(err);
             const data = await word2pdf(`./uploads/${req.body.name}`);
             const fileName = req.body.name.split('.')[0];
             await helper.writeToFile(res, './converted', `${fileName}.pdf`, data);
-            return res.status(200).json({ message: 'File converted successfully' });
+            return res.status(200).json({ message: 'File converted successfully', name: `${fileName}.pdf` });
         });
     } catch (err) {
         return res.status(500).json({ message: err });
     }
+};
+
+exports.DownloadFile = async(req, res) => {
+    const file = path.join(__dirname, '../converted/') + '/' + req.params.fileName;
+    await res.sendFile(file)
 };
--- PDF-backend/routes/WordRoutes.js
+++ PDF-backend/routes/WordRoutes.js
@@ -3,6 +3,8 @@ const WordCtrl = require('../controllers/WordCtrl');
 
 const router = express.Router();
 
+router.get('/word-to-pdf/:fileName', WordCtrl.DownloadFile);
+
 router.post('/word-to-pdf', WordCtrl.WordToPdf);
 
 module.exports = router;
